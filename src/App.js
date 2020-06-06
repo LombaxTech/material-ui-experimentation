78ee72c269c13b7e45a3beeb842d5aa0e8becfa2
Rakib Khan <rakibkhan@live.co.uk>
2020-06-07 00:05:28 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const style = {
+  height: '500px',
+  width: '260px',
+  background: 'green'
 }
+
+const App = () => (
+
+  <div>
+    <Button
+      color="secondary"
+      fullWidth={true}
+      href="https://www.youtube.com/watch?v=33gE0EYKesc"
+      variant='contained'
+    >
+      Rainych Song
+    </Button>
+    <div style={style}>
+
+    </div>
+  </div>
+)
 
 export default App;
