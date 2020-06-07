fbc0a0a0a7e133cf1f1212bc7d01714976da949e
Rakib Khan <rakibkhan@live.co.uk>
2020-06-07 20:42:59 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const style = {
-  height: '500px',
-  width: '260px',
-  background: 'green'
+
+const useStyles = makeStyles({
+  greetingStyle: {
+    textDecoration: 'underline'
+  }
+});
+
+const App = () => {
+
+  const classes = useStyles();
+  return (
+    <div>
+
+      <Typography
+        color="secondary"
+        variant="h2"
+        className={classes.greetingStyle}
+      >
+        A link to Rainych's song
+    </Typography>
+      <Button
+        color="secondary"
+        fullWidth={true}
+        href="https://www.youtube.com/watch?v=33gE0EYKesc"
+        variant='contained'
+      >
+        Rainych Song
+    </Button>
+    </div>
+  )
 }
 
-const App = () => (
-
-  <div>
-    <Button
-      color="secondary"
-      fullWidth={true}
-      href="https://www.youtube.com/watch?v=33gE0EYKesc"
-      variant='contained'
-    >
-      Rainych Song
-    </Button>
-    <div style={style}>
-
-    </div>
-  </div>
-)
-
 export default App;
+
+
+
+// const style = {
+//   height: '500px',
+//   width: '260px',
+//   background: 'green'
+// }
